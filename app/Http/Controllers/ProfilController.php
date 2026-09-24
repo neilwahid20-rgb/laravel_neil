e188e6433ef6bca1edaab9d5cb4e2a93c7bb9798
neilwahid20-rgb <neilwahid20@gmail.com>
2026-09-24 07:44:29 +0700

    Add files via upload

---
+++ app/Http/Controllers/ProfilController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfilController extends Controller
+{
+    public function index()
+    { 
+   $nama = "Neil";
+$kelas = "XII RPL";
+$hobi = "Game, Bermain bola, Bernyanyi";
+
+
+        return view('profil', [
+            'nama' => $nama,
+            'kelas' => $kelas,
+            'hobi' => $hobi,
+        ]);
+    }
+
+    public function sapa($nama)
+    {
+        return view('sapa', ['nama' => $nama]);
+    }
+
+    public function mapel()
+    {
+        $daftarMapel = ['Pemrograman Web', 'Basis Data', 'PBO', 'Bahasa Indonesia', 'Matematika'];
+        return view('mapel', ['mapel' => $daftarMapel]);
+    }
+}
